a9af8844950008ca117528b0065d98398cf45e18
Jean F. dos Santos <jeanddg@hotmail.com>
2022-11-29 17:06:41 -0300

    🎉 setting up the project

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Header } from './components/Header';
+import { Order } from './screens/Order';
+import { GlobalStyles } from './styles/GlobalStyles';
+
+function App() {
+  return (
+    <>
+      <GlobalStyles />
+      <Header />
+      <Order />
+    </>
+  );
+}
+
+export default App;
